477fdde009441b2ff876eccbba60d9fb3b88c039
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-11-03 11:32:43 +0100

    Push

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ Prodotto iphone = new Prodotto("Iphone", "telefono molto costoso", 133333.33f);
 
 Console.WriteLine("Codice Iphone : " + iphone.Codice);
 Console.WriteLine("Prezzo Iphone : " +  iphone.Prezzo + " $");
-Console.WriteLine("Prezzo Iphone ivato : " + iphone.GetPriceWithIva() + " $");
-Console.WriteLine("Nome prodotto integrale : " + iphone.GetFullName());
-Console.WriteLine("Codice pad left : " + iphone.GetPadLeftCodice());
+Console.WriteLine("Prezzo Iphone ivato : " + iphone.PriceWithIva + " $");
+Console.WriteLine("Nome prodotto integrale : " + iphone.FullName);
+Console.WriteLine("Codice pad left : " + iphone.PadLeftCodice);
 
 public class Prodotto
 {
@@ -18,6 +18,46 @@ public class Prodotto
     public double Prezzo { get; set; }
     public int Iva { get; set; }
 
+    public double PriceWithIva { 
+        get 
+        {
+            return (this.Prezzo * this.Iva) / 100;
+        } 
+    }
+
+    public string FullName
+    {
+        get
+        {
+            return this.Nome + this.Codice;
+        }
+    }
+
+    public string PadLeftCodice
+    {
+        get
+        {
+            //funzione PadLeft di c#
+
+            /*        
+             * char pad = '0';
+             * return codice.ToString().PadLeft(8, pad);
+            */
+
+            //funzione alternativa fatta a mano
+
+            string codicePadLeft = Codice.ToString();
+
+            for (int i = 0; codicePadLeft.Length < 8; i++)
+            {
+                codicePadLeft = "0" + codicePadLeft;
+            }
+
+            return codicePadLeft;
+        }
+    }
+
+
     // construttore
     public Prodotto(string nome, string descrizione, float prezzo)
     {
@@ -27,38 +67,5 @@ public class Prodotto
         this.Descrizione = descrizione;
         this.Prezzo = prezzo;
         Iva = 22;
-    }
-    
-    // metodi
-
-    public double GetPriceWithIva()
-    {
-        return (this.Prezzo * this.Iva) / 100;
-    }
-
-    public string GetFullName()
-    {
-        return this.Nome + this.Codice;
-    }
-
-    public string GetPadLeftCodice()
-    {
-        //funzione PadLeft di c#
-
-        /*        
-         * char pad = '0';
-         * return codice.ToString().PadLeft(8, pad);
-        */
-
-        //funzione alternativa fatta a mano
-
-        string codicePadLeft = Codice.ToString();
-
-        for (int i = 0; codicePadLeft.Length < 8; i++)
-        {
-            codicePadLeft = "0" + codicePadLeft;
-        }
-
-        return codicePadLeft;
     }
 }
